d5fa0afbe7f4bd88c5e1505a489d74ce4c9efa00
aung <aung@a2mbp.local>
2019-10-24 13:22:48 -0700

    next button state

--- src/Screens/LobbyPage.js
+++ src/Screens/LobbyPage.js
@@ -4,14 +4,43 @@ import styled from 'styled-components';
 import { Container, Header, Logo, Input, Label, Button } from './styles';
 
 class LobbyPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      done: false,
+    };
+    this.handleNextClick = this.handleNextClick.bind(this);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.setState({ name: event.target.value });
+  }
+
+  handleNextClick() {
+    const { name } = this.state;
+    //console.log('next clicked ' + name);
+    if (name) {
+      this.setState({ done: true });
+    }
+  }
+
   render() {
+    const { name, done } = this.state;
     return (
       <Container>
         <Header>Lobby Page</Header>
-        <Logo src="whitelogo.png"/>
-        <Label>Enter your name</Label>
-        <Input/>
-        <Button>Next</Button>
+        <Logo src="whitelogo.png" />
+        {!done ?
+          <div>
+            <Label>Enter your name</Label>
+            <Input onChange={this.handleNameChange} />
+            <Button onClick={this.handleNextClick}>Next</Button>
+          </div>
+          :
+          <Label>{name}</Label>
+        }
         <Link to="/quiz">Quiz Page</Link>
       </Container>
     );
