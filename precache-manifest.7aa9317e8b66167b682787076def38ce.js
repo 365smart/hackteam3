2e727a17751b1dc30bd5556235dadc1ddbc1b502
Ron Nielsen <ronaldnielsen@gmail.com>
2019-10-24 20:39:22 -0700

    Updates

--- precache-manifest.7aa9317e8b66167b682787076def38ce.js
+++ precache-manifest.7aa9317e8b66167b682787076def38ce.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7f2f59286a087fd6cd214599138a7df2",
+    "revision": "8797ecad5bd0d82097657edacfc9f801",
     "url": "/hackteam3/index.html"
   },
   {
-    "revision": "4d1148b0c32b80558d6a",
+    "revision": "fd2d0da59dd566841bf8",
     "url": "/hackteam3/static/css/main.6b97dc4a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hackteam3/static/js/2.08e4ba9a.chunk.js"
   },
   {
-    "revision": "4d1148b0c32b80558d6a",
-    "url": "/hackteam3/static/js/main.8829a08f.chunk.js"
+    "revision": "fd2d0da59dd566841bf8",
+    "url": "/hackteam3/static/js/main.9c90997f.chunk.js"
   },
   {
     "revision": "f863b9013a709be5f5a0",
