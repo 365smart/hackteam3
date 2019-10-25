16471d2156058d68423e3068e7b9f81c26a7f474
Ron Nielsen <ronaldnielsen@gmail.com>
2019-10-24 20:03:56 -0700

    Updates

--- precache-manifest.be928e47845dd4a8dff49e83c5e53e1c.js
+++ precache-manifest.be928e47845dd4a8dff49e83c5e53e1c.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "95a197f41afcb76bddb6a303d2636d41",
+    "revision": "29b990da5f86adfd3e3591efeff97119",
     "url": "/hackteam3/index.html"
   },
   {
-    "revision": "99ca69c5fafe535706a5",
+    "revision": "d8ef7975e6224fe9a676",
     "url": "/hackteam3/static/css/main.6b97dc4a.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/hackteam3/static/js/2.08e4ba9a.chunk.js"
   },
   {
-    "revision": "99ca69c5fafe535706a5",
-    "url": "/hackteam3/static/js/main.b1a83812.chunk.js"
+    "revision": "d8ef7975e6224fe9a676",
+    "url": "/hackteam3/static/js/main.2b90db91.chunk.js"
   },
   {
     "revision": "f863b9013a709be5f5a0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hackteam3/precache-manifest.ff2915e75fd3039ce385e76bab906d12.js"
+  "/hackteam3/precache-manifest.be928e47845dd4a8dff49e83c5e53e1c.js"
 );
 
 self.addEventListener('message', (event) => {
